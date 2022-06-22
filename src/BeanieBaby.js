85b644ae3766bbfbafae0705cb69efe8567c03c6
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-22 12:13:50 -0700

    building out beanie descriptor page

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -1,9 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getBeanieBabies } from './services/fetch-utils';
 
 export default function BeanieBaby({ beanieBaby }) {
+  const params = useParams();
+  const [beanieBaby, setBeanieBaby] = useState({});
+
+  useEffect(() => {
+    async function fetchBeanie() {
+      const data = await getBeanieBabies(params.id);
+      setBeanieBaby(data);
+    }
+    fetchBeanie();
+  }, [params.id]);
+
   return (
+<div>
+  <h2>{beanieBaby.title}</h2>
+</div>
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    
   );
 }
